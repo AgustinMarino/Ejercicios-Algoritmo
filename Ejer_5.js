d653fd1d1f3d9c73cc78835292c6d7eb53b32ec6
AgustinMarino <agustinemarino@gmail.com>
2023-06-14 15:04:14 -0300

    pp

--- Ejer_5.js
+++ Ejer_5.js
@@ -7,12 +7,3 @@ function Ejercicio5() {
     console.log(aux);
 }
 
-
-function Ejercicio6(params) {
-    var num = document.querySelector("#parOImpar").value;
-    if (num % 2 == 0) {
-        console.log("El número es par");
-    } else {
-        console.log("El número es impar");
-    }
-}
---
+++ Ejer_6.js
@@ -0,0 +1,8 @@
+function Ejercicio6(params) {
+    var num = document.querySelector("#numero").value;
+    if (num % 2 == 0) {
+        console.log("El número es par");
+    } else {
+        console.log("El número es impar");
+    }
+}
