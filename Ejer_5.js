73fc55c709adf01900beca549850573665ce39ec
AgustinMarino <132932937+AgustinMarino@users.noreply.github.com>
2023-07-14 17:50:38 -0300

    p

--- Ejer_5.js
+++
@@ -1,9 +0,0 @@
-function Ejercicio5() {
-    var entero = document.querySelector("#entero").value;
-    var aux = 1;
-    for (let i = entero; i > 0; i--) {
-        aux = aux * i;
-    }
-    console.log(aux);
-}
-
--- Ejer_6.js
+++
@@ -1,8 +0,0 @@
-function Ejercicio6(params) {
-    var num = document.querySelector("#numero").value;
-    if (num % 2 == 0) {
-        console.log("El número es par");
-    } else {
-        console.log("El número es impar");
-    }
-}
